25a9d43ff1ddf691202fd5a0ce24796c2929d997
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-03-09 10:23:34 +0600

    Added option element to the select

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // init synthesis
-
 const synth = window.speechSynthesis;
 
 // Dom Elements
@@ -10,3 +9,36 @@ const rate = document.querySelector("#rate");
 const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
+
+
+// init voices array
+let voices = [];
+
+// async const getVoices = () => {
+//     voices = synth.getVoices();
+//     console.log(voices);
+
+// }
+
+const getVoices = () => {
+    voices = synth.getVoices();
+    if (voices.length !== 0) {
+        // console.log("start loading voices");
+        console.log(voices);
+
+        // Loop through voices and create option element
+        voices.forEach(voice => {
+            const option = document.createElement("option");
+            // Fill option with voice and language
+            option.textContent = voice.name + "(" + voice.lang + ")";
+            // Set needed option attribute
+            option.setAttribute("data-lang", voice.lang);
+            option.setAttribute("data-name", voice.name);
+            // Add to select list
+            voiceSelect.appendChild(option);
+        })
+    } else {
+        setTimeout(() => getVoices(), 1)
+    }
+}
+getVoices();
