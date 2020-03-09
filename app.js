f201a73cff4ee5129a84dc7ccedf112a3c3e2b30
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-03-09 09:48:28 +0600

    init synthesis and select dom elements

---
+++ app.js
@@ -0,0 +1,12 @@
+// init synthesis
+
+const synth = window.speechSynthesis;
+
+// Dom Elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
